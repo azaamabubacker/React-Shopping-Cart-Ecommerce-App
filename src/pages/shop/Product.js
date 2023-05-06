8ccea9a58d24bfa37722efcf6aa64f5475c1471d
azaamabubacker <cleancodelk@gmail.com>
2023-05-06 14:22:31 +0530

    Add cart functionality using contextAPI

--- src/pages/shop/Product.js
+++ src/pages/shop/Product.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Product.module.css";
+import { ShopContext } from "../../context/shop-context";
 
 function Product(props) {
   const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const addCartHandler = () => {
+    addToCart(id);
+  };
+
+  const cartItemsAmount = cartItems[id];
 
   return (
     <div className={classes.product}>
@@ -14,6 +22,9 @@ function Product(props) {
         </p>
         <p>{price}</p>
       </div>
+      <button onClick={addCartHandler} className={classes.addToCartBttn}>
+        Add to Cart {cartItemsAmount > 0 && cartItemsAmount}
+      </button>
     </div>
   );
 }
